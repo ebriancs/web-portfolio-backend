5d6cbb6febcda0aadacbc4de637cf10c195822d0
ebriancs <ebrian.cs@gmail.com>
2025-01-19 23:15:39 +0800

    UPDATED

--- api/admin.py
+++ api/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import DeviceInfo
 
 class DeviceInfoAdmin(admin.ModelAdmin):
-    list_display = ('user_agent', 'platform', 'language', 'ip', 'created_at')
-    search_fields = ('user_agent', 'platform', 'language', 'ip')
-    list_filter = ('platform', 'language', 'created_at')
+    list_display = ('user_agent', 'platform', 'ip', 'referrer', 'created_at')
+    search_fields = ('user_agent', 'platform', 'language', 'ip', 'referrer')
+    list_filter = ('platform', 'created_at')
     ordering = ('-created_at',)
 
 admin.site.register(DeviceInfo, DeviceInfoAdmin)
