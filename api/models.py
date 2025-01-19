405e2acc71e03b9a131bb942b89a79fa2c3790c8
ebriancs <ebrian.cs@gmail.com>
2025-01-19 23:11:35 +0800

    UPDATED

--- api/models.py
+++ api/models.py
@@ -3,10 +3,10 @@ from django.db import models
 from django.db import models
 
 class DeviceInfo(models.Model):
-    user_agent = models.TextField()
-    platform = models.CharField(max_length=100)
-    language = models.CharField(max_length=50)
-    ip = models.GenericIPAddressField()
+    user_agent = models.TextField(blank=True, null=True)
+    platform = models.CharField(max_length=100, blank=True, null=True)
+    language = models.CharField(max_length=50, blank=True, null=True)
+    ip = models.GenericIPAddressField(blank=True, null=True)
     referrer = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
