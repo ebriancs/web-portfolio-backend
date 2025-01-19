fbcd8204441dae3fcbb08af7cbe1ab9fcbba021d
ebriancs <ebrian.cs@gmail.com>
2025-01-20 02:27:41 +0800

    UPDATED

--- api/views.py
+++ api/views.py
@@ -49,7 +49,7 @@ class ContactView(APIView):
             serializer = ContactSerializer(data=mapped_data)
             if serializer.is_valid():
                 serializer.save()
-                return Response({'message': 'Contact created successfully!'}, status=status.HTTP_201_CREATED)
+                return Response({'message': 'Message sent successfully!'}, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
