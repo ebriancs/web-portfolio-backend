5fe2d8b874a4f8dd7f447b2236363c9be1571757
ebriancs <ebrian.cs@gmail.com>
2025-01-29 20:36:03 +0800

    updated

--- backend/settings.py
+++ backend/settings.py
@@ -164,16 +164,13 @@ REST_FRAMEWORK = {
 }
 
 # Email settings
-EMAIL_SETTINGS_ON = config('EMAIL_SETTINGS_ON', default=False, cast=bool)
-
-if EMAIL_SETTINGS_ON:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = config('EMAIL_HOST', default=None)
-    EMAIL_PORT = config('EMAIL_PORT', default=None, cast=int)
-    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default=None)
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=None)
-    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=None)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default=None)
+EMAIL_PORT = config('EMAIL_PORT', default=None, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default=None)
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=None)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=None)
 
 
 # Security settings
